d37ebf07644d71fc91c98d2fe168ac21e5abdf1b
Lennard Siebke <lennard.siebke@grnstn.net>
2023-02-01 12:21:39 +0100

    change SingleBlog view

--- src/templates/SingleBlog.vue
+++ src/templates/SingleBlog.vue
@@ -1,7 +1,15 @@
 <template>
   <Layout>
+    <div class="featuredImageWrapper">
+      <img class="featuredImage parallax-bg" :src="$context.featuredMedia.sourceUrl" :title="$context.featuredMedia.title" :alt="$context.featuredMedia.altText" >
+      <div class="featuredImageOverlay" />
+      <BaseTitle :align="'left'" :tag="'h1'" class="featuredTitle">
+        <span class="animateFadeInLeft" style="font-weight: 400">{{ getFeaturedTitle()[0] }}</span><br>
+        <span class="greenColor animateFadeInLeft">{{ getFeaturedTitle()[1] }}</span><br>
+        <g-image style="margin-top: 20px; width: 50px;" class="animateFadeInLeftSecond" src="@/assets/Pfeile.svg"></g-image>
+      </BaseTitle>
+    </div>
     <div class="mainSection">
-      <BaseTitle :align="'left'" :tag="'h1'" class="defaultMarginX defaultMarginHead">{{ $context.title }}</BaseTitle>
       <div class="metaData">
         <p class="small greenColor">{{ getFormattedDate($context.date) }}</p>
         <div class="socialShare">
@@ -11,11 +19,17 @@
         </div>
       </div>
     </div>
-    <div class="featuredImageWrapper">
-      <img class="featuredImage parallax-bg" :src="$context.featuredMedia.sourceUrl" :title="$context.featuredMedia.title" :alt="$context.featuredMedia.altText" >
-    </div>
-    <div class="mainSection">
+    <div class="mainSection contentWrapper">
       <div class="singleContent" v-html="$context.content"></div>
+      <div class="sidebarBlog pinnedContainer">
+        <a class="singleBlog" :href="'/blog/' + edge.node.slug" v-for="edge in $static.posts.edges" :key="edge.node.id">
+          <div class="singleCard">
+            <img class="featured" v-if="edge.node.featuredMedia != null" :src="edge.node.featuredMedia.sourceUrl" :title="edge.node.featuredMedia.title" :alt="edge.node.featuredMedia.altText">
+            <p v-html="edge.node.title">
+            <p style="font-size: 18px" class="greenColor" v-html="edge.node.date"></p>
+          </div>
+        </a>
+      </div>
     </div>
   </Layout>
 </template>
@@ -66,6 +80,15 @@ export default {
             scrub: 1,
           }
         });
+        gsap.to(".pinnedContainer", {
+          ease: "none", // <-- IMPORTANT!
+          scrollTrigger: {
+            trigger: ".pinnedContainer",
+            pin: true,
+            scrub: 1,
+            invalidateOnRefresh: true,
+          }
+        });
       },
       "(max-width: 1024px)": () => {
       },
@@ -94,8 +117,7 @@ export default {
           scrollTrigger: {
             scrub: true
           },
-          scale: () => 1.3,
-          opacity: 0,
+          y: () => ScrollTrigger.maxScroll(window),
           ease: "none"
         });
       }
@@ -105,6 +127,9 @@ export default {
     getFormattedDate(date){
       const newDate = new Date(date);
       return newDate.toLocaleDateString('de-de');
+    },
+    getFeaturedTitle(){
+      return this.$context.title.split("*");
     }
   },
   metaInfo() {
@@ -137,14 +162,38 @@ export default {
   margin-left: 10px;
 }
 .featuredImageWrapper{
-  margin: 50px 0px;
+  margin: 80px 0px 50px 0px;
   width: 100%;
-  max-height: 600px;
+  max-height: 1000px;
   overflow: hidden;
+}
+.featuredImageOverlay{
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(to right, #011713 20%, rgba(0,0,0,0) 60%);
 }
 img.featuredImage{
   width: 100%;
   max-height: 1000px;
+}
+.featuredTitle{
+  position: absolute;
+  top: 30%;
+  left: 10%;
+  max-width: 30%;
+}
+.contentWrapper{
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 30px;
+}
+div.sidebarBlog{
+  padding-top: 100px;
 }
 </style>
 
@@ -156,3 +205,22 @@ div.singleContent h2, div.singleContent h3{
   margin-bottom: 20px;
 }
 </style>
+
+<static-query>
+query{
+  posts: allWordPressPost(limit: 3){
+    edges{
+      node{
+        slug,
+        title,
+        date,
+        featuredMedia{
+          sourceUrl,
+          title,
+          altText
+        }
+      }
+    }
+  }
+}
+</static-query>
