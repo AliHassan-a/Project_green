d7415bf2e1c48914a3d7cb65160fd95a0c0ee884
renegreenstein <129854415+renegreenstein@users.noreply.github.com>
2023-04-20 23:54:13 +0200

    Lauffähiger Build

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,34 +1,28 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
   siteName: 'Greenstein Designagentur',
   siteUrl: 'https://greenstein.design',
   plugins: [
-      {
-        use: '@gridsome/source-wordpress',
-        options: {
-          baseUrl: 'https://admin.greenstein.design', // required
-          apiBase: 'wp-json',
-          typeName: 'WordPress',
-          perPage: 100,
-          concurrent: 10
-        }
-      },
-      {
-        use: '@gridsome/plugin-sitemap',
-        options: {
-          config: {
-            '/blog/*': {
-              changefreq: 'weekly',
-              priority: 0.5,
-              lastmod: '2023-03-07',
-            },
-          }
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'https://admin.greenstein.design', // erforderlich
+        apiBase: 'wp-json',
+        typeName: 'WordPress', // hinzufügen
+        perPage: 100,
+        concurrent: 10,
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        config: {
+          '/blog/*': {
+            changefreq: 'weekly',
+            priority: 0.5,
+            lastmod: '2023-03-07',
+          },
         }
       }
-    ]
+    }
+  ]
 }
